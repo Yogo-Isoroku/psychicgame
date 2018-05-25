1653c424a3aca285b918526e85fd6cabc5f9020f
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-05-25 11:44:44 -0700

    completed program

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,65 @@
 var wins = 0;
 var losses = 0;
-var numGuess = 9;
-
+var guessesRemaining = 9;
+var lettersGuessed = [];
+// computer choices for random secretLetter varible
 var compGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
-// Determines which key was pressed. Will start the game.
-document.onkeyup = function newGame(anyKey) {
-        $("win").text("");
-        let guesses = 9;
-        
-        let secretLetter = compGuess[Math.floor(Math.random() * compGuess.length)];
-
+//  Will start the game.
+document.onkeyup = function newGame(anykey) {
+       
+    
+    var secretLetter = compGuess[Math.floor(Math.random() * compGuess.length)];
    
 
-    // letters guessed array
-    let lettersGuessed = [];
-    
-    
-
+    var lettersFound = 0;
+// User input for guesses
     document.onkeyup = function (e) {
         let guess = e.key;
 
+       
+    // letters guessed array
+    var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-        // push guess into lettersGuessed[]
-        lettersGuessed.push(String(guess.toLowerCase()));
-
-        // use lettersFound === currentWordArray.length as win condition
-        let lettersFound = 0;
-
-
-        // check for correct guess. iterate either letters found or guesses.
-        for (let i = 0; i < secretLetter.length; i++) {
-            if (secretLetter[i].charAt(0) === guess) {
-                lettersFound++;
-            };
-        };
-
-        // console feedback. change to alert or html at some point
-        if (lettersFound > 0) {
-            
-            guess.length = 0;
-            // document.location.reload();
-            wins++;
-            newGame();
-            alert("You win!");
-
-        } else {
-            guesses--;
-        };
-        // console.log('current guesses: ' + lettersGuessed.toLocaleString());
-
-        if (guesses < 1) { //game over and reset. change from alert to high z-index html 
-           
-            // document.location.reload();
-            losses++;
-            newGame();
-            alert("YOU LOSE!");
+    
+   for (let i = 0; i < secretLetter.length; i++) {
+       if (secretLetter[i].charAt(0) === guess) {
+            lettersFound++;
         }
-        
-         
+    }// keeps random letter choice from changing on keyup
 
-        // $(document).ready(function () {
-        //     $('#but').click(newGame);
-        // });
-        //adds guesses to HTML
-        $('#guessesMade').text('Your Guesses so far:' + ' ' + lettersGuessed + ', ');
-        $('.wins').text('Wins:' + ' ' + wins);
-        $('.losses').text('Losses:' + ' ' + losses);
-        $('.guesses-remaining').text('Guesses Remaining:' + ' ' + guesses);
+    if (choices.indexOf(guess) > -1) {
+        // win directions
+        if (lettersFound > 0) { 
+            wins++;
+            guessesRemaining = 9;
+            lettersGuessed = [];
+            newGame(); //calls for a new random letter
+        }
 
-    };
+        else {
+            guessesRemaining--;
+            lettersGuessed.push(guess);
+        }
+       
+       // loss directions
+        if (guessesRemaining === 0) { 
+           losses++;
+           guessesRemaining = 9;
+           lettersGuessed = [];
+           newGame(); //calls for a new random letter
+           
+        }
+    }
+
+    
+    //adds guesses to HTML
+    $('#guessesMade').text('Your Guesses so far: ' + lettersGuessed);
+    $('.wins').text('Wins:' + ' ' + wins);
+    $('.losses').text('Losses:' + ' ' + losses);
+    $('.guesses-remaining').text('Guesses Remaining:' + ' ' + guessesRemaining);
+    // console.log(lettersGuessed);
+    // console.log(secretLetter);
+    }
 };
