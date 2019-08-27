bb96d10e3db17f1300088305290e5588b2e898c6
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-08-27 09:53:27 -0700

    updated all files. ReadME: added How to play. Other files: removed console.logs, unneeded comments in code and reformatted. Removed empty files and folders.

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # psychicgame
 
-A guessing game using HTML, CSS, and Javascript. 
-App chooses a random letter. player has 9 tries to match the hidden value. 
-If correct app updates user win and chooses a new letter. 
-if incorrect app updates user loss and selects a new letter.
+Guess the letter the computer is thinking of.
+
+## How to play
+
+- The Computer chooses a random letter.
+- Player has 9 tries to match the hidden value.
+- If correct app updates user win and chooses a new letter.
+- If incorrect app updates user loss and selects a new letter.
+
+## Tools
+
+- HTML
+- CSS
+- Javascript.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,63 +3,105 @@ var losses = 0;
 var guessesRemaining = 9;
 var lettersGuessed = [];
 // computer choices for random secretLetter varible
-var compGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+var compGuess = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+];
 
 //  Will start the game.
 document.onkeyup = function newGame(anykey) {
-       
-    
-    var secretLetter = compGuess[Math.floor(Math.random() * compGuess.length)];
-   
+	var secretLetter = compGuess[Math.floor(Math.random() * compGuess.length)];
 
-    var lettersFound = 0;
-// User input for guesses
-    document.onkeyup = function (e) {
-        let guess = e.key;
+	var lettersFound = 0;
+	// User input for guesses
+	document.onkeyup = function(e) {
+		let guess = e.key;
 
-       
-    // letters guessed array
-    var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+		// letters guessed array
+		var choices = [
+			'a',
+			'b',
+			'c',
+			'd',
+			'e',
+			'f',
+			'g',
+			'h',
+			'i',
+			'j',
+			'k',
+			'l',
+			'm',
+			'n',
+			'o',
+			'p',
+			'q',
+			'r',
+			's',
+			't',
+			'u',
+			'v',
+			'w',
+			'x',
+			'y',
+			'z',
+		];
 
-    
-   for (let i = 0; i < secretLetter.length; i++) {
-       if (secretLetter[i].charAt(0) === guess) {
-            lettersFound++;
-        }
-    }// keeps random letter choice from changing on keyup
+		for (let i = 0; i < secretLetter.length; i++) {
+			if (secretLetter[i].charAt(0) === guess) {
+				lettersFound++;
+			}
+		} // keeps random letter choice from changing on keyup
 
-    if (choices.indexOf(guess) > -1) {
-        // win directions
-        if (lettersFound > 0) { 
-            wins++;
-            guessesRemaining = 9;
-            lettersGuessed = [];
-            newGame(); //calls for a new random letter
-        }
+		if (choices.indexOf(guess) > -1) {
+			// win directions
+			if (lettersFound > 0) {
+				wins++;
+				guessesRemaining = 9;
+				lettersGuessed = [];
+				newGame(); //calls for a new random letter
+			} else {
+				guessesRemaining--;
+				lettersGuessed.push(guess);
+			}
 
-        else {
-            guessesRemaining--;
-            lettersGuessed.push(guess);
-        }
-       
-       // loss directions
-        if (guessesRemaining === 0) { 
-           losses++;
-           guessesRemaining = 9;
-           lettersGuessed = [];
-           newGame(); //calls for a new random letter
-           
-        }
-    }
+			// loss directions
+			if (guessesRemaining === 0) {
+				losses++;
+				guessesRemaining = 9;
+				lettersGuessed = [];
+				newGame(); //calls for a new random letter
+			}
+		}
 
-    
-    //adds guesses to HTML
-    $('#guessesMade').text('Your Guesses so far: ' + lettersGuessed);
-    $('.wins').text('Wins:' + ' ' + wins);
-    $('.losses').text('Losses:' + ' ' + losses);
-    $('.guesses-remaining').text('Guesses Remaining:' + ' ' + guessesRemaining);
-    // console.log(lettersGuessed);
-    // console.log(secretLetter);
-    }
+		//adds guesses to HTML
+		$('#guessesMade').text(lettersGuessed);
+		$('.wins').text(wins);
+		$('.losses').text(losses);
+		$('.guesses-remaining').text(guessesRemaining);
+	};
 };
